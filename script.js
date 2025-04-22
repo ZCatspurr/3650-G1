14ccb2fe92452ff18835e7a43b5903e7bfb38ab3
ZCatspurr <160622887+ZCatspurr@users.noreply.github.com>
2025-04-22 14:01:04 -0400

    Small line change

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 document.getElementById("joinForm").addEventListener("submit", function (e) {
     e.preventDefault();
-  
-        alert("Just so we have something to build on.");
+        alert("Successfully added to room.");
   });
   
